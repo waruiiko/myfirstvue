c4af655152ca0906e1b27572cd73d9485d4621a0
GinBe <asdfwxwx@gmail.com>
2021-07-27 23:17:41 +0800

    210727

---
+++ tests.js
@@ -0,0 +1,5 @@
+// const hello = (name:string) => {
+//     return `hello! ${name}`;
+// }
+// hello('xiaoming')
+console.log("hello");
---
+++ tests.ts
@@ -0,0 +1,7 @@
+// const hello = (name:string) => {
+//     return `hello! ${name}`;
+// }
+
+// hello('xiaoming')
+
+console.log("hello")
