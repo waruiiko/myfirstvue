bb4c5028cbd68a756afaeefeae3248b5965e5f97
GinBe <asdfwxwx@gmail.com>
2021-07-29 16:29:56 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # myfirstvue
 https://www.youtube.com/watch?v=Z8xVqol9n0k&amp;list=PL_Ykv8s0HissGwdOz4nQASmRyHt19DOms&amp;index=2
+
+## 从零到一开发到上线 高仿知乎专栏
+## 使用 Typescript + Vue3 开发
+http://docs.vikingship.xyz/
--- test.ts
+++ test.ts
@@ -14,16 +14,66 @@
 // arr.splice(2)
 // console.log(arr)
 
-interface IPersons {
-    readonly id:number;
-    name:string;
-    age?:number;  // 加?代表该属性可选
-}
+// interface IPersons {
+//     readonly id:number;
+//     name:string;
+//     age?:number;  // 加?代表该属性可选
+// }
 
-let person:IPersons ={
-    id:12,
-    name:'xiaoming',
-    age:30
-}
+// let person:IPersons ={
+//     id:12,
+//     name:'xiaoming',
+//     age:30
+// }
 
+/* 2 8 
+类型推论 
+联合类型和 
+类型断言
+*/
 
+// //获取长度
+// function getLength(input:string|number): number{
+//     const str = input as string;
+
+//     //如果输入的值为字符串，则直接获得长度，否则先转换成字符串，再获得长度
+//     if(str.length){
+//         return str.length;
+//     }else{
+//         const number = input as number;
+//         return number.toString().length
+//     }
+// }
+
+// //type guard 类型守护
+// function getLength2(input:string|number):number{
+//     if (typeof input === 'string'){
+//         return input.length;
+//     } else{
+//         return input.toString().length;
+//     }
+// } 
+
+// Class 类
+// 面向对象编程的三大特点
+
+// 封装（Encapsulation）：将对数据的操作细节隐藏起来，只暴露对外的接口。外界调用端不需要（也不可能）知道细节，就能通过对外提供的接口来访问该对象，
+// 继承（Inheritance）：子类继承父类，子类除了拥有父类的所有特性外，还有一些更具体的特性。
+// 多态（Polymorphism）：由继承而产生了相关的不同的类，对同一个方法可以有不同的响应。
+
+class Animal {
+    name: string;
+    //构造函数：实例化时进行的逻辑
+    constructor(sname: string) {
+      this.name = sname
+    }
+    run() {
+      return `${this.name} is running`
+    }
+    eat() {
+        return `${this.name} is eating` 
+    }
+  }
+  const snake = new Animal('lily')
+  console.log(snake.run())
+  console.log(snake.eat())
