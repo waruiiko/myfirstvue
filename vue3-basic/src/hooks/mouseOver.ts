5b446911ee77f29c8ee49044c783f1d4d5f5d8f0
GinBe <asdfwxwx@gmail.com>
2021-08-03 14:47:15 +0800

    Create mouseOver.ts

---
+++ vue3-basic/src/hooks/mouseOver.ts
@@ -0,0 +1,17 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+function myMouseOver() {
+  const m = ref(0);
+  const n = ref(0);
+  const updateMouse = (e: MouseEvent) => {
+    m.value = e.pageX;
+    n.value = e.pageY;
+  };
+  onMounted(() => {
+    document.addEventListener("mouseover", updateMouse);
+  });
+  onUnmounted(() => {
+    document.removeEventListener("mouseover", updateMouse);
+  });
+  return { m, n }
+}
+export default myMouseOver;
