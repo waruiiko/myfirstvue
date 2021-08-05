050899aba134de9a0222747dc770414b38946caa
GinBe <asdfwxwx@gmail.com>
2021-08-05 16:09:03 +0800

    onErrorCaptured

--- vue3-basic/src/App.vue
+++ vue3-basic/src/App.vue
@@ -2,44 +2,55 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  <router-view/>
-  <HelloWorld/>
-  <button @click="openModal">modal</button>
-  <modal :isOpen="modalIsOpen" @close-modal="onModalClose">MyModal!</modal>
-  <Suspense>
-    <template #default>
-      <async-show/>
-    </template>
-    <template #fallback>
-      Loading...
-    </template>
-  </Suspense>
+    <router-view />
+    <HelloWorld />
+    <button @click="openModal">modal</button>
+    <modal :isOpen="modalIsOpen" @close-modal="onModalClose">MyModal!</modal>
+    <p>{{ err }}</p>
+    <Suspense>
+      <template #default>
+        <div>
+          <async-show />
+          <dog-show />
+        </div>
+      </template>
+      <template #fallback> Loading... </template>
+    </Suspense>
   </div>
 </template>
 
 <script>
-import modal from './components/Modal.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import asyncShow from './components/AsyncShow.vue'
-import {defineComponent,ref} from 'vue'
+import modal from "./components/Modal.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import AsyncShow from "./components/AsyncShow.vue";
+import DogShow from "./components/DogShow.vue";
+import { defineComponent, ref, onErrorCaptured } from "vue";
 export default defineComponent({
-  components:{modal,HelloWorld,asyncShow},
+  components: { modal, HelloWorld, AsyncShow, DogShow },
 
-  setup(){
-    const modalIsOpen = ref(false)
-    const openModal =()=>{
-      modalIsOpen.value = true
-    }
-    const onModalClose =()=>{
-      modalIsOpen.value = false
-    }
+  setup() {
+    //抓取错误（修改DogShow.vue组件的链接）
+    const err = ref(null);
+    onErrorCaptured((error) => {
+      err.value = error;
+      return true;
+    });
+
+    const modalIsOpen = ref(false);
+    const openModal = () => {
+      modalIsOpen.value = true;
+    };
+    const onModalClose = () => {
+      modalIsOpen.value = false;
+    };
     return {
       modalIsOpen,
       openModal,
-      onModalClose
-    }
-  }
-})
+      onModalClose,
+      err,
+    };
+  },
+});
 </script>
 
 <style>
